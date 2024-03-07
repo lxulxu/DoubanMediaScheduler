f6d0e98372c818f4f46ed95132a5b3c4d66a28cf
lxulxu <lxulxu1994@163.com>
2024-03-07 15:00:46 +0800

    update

--- douban_calendar_generator.py
+++ douban_calendar_generator.py
@@ -2,7 +2,7 @@
 Author: lxulxu
 Date: 2024-03-01 15:21:33
 LastEditors: lxulxu
-LastEditTime: 2024-03-07 09:22:20
+LastEditTime: 2024-03-07 14:59:57
 Description: 
 
 Copyright (c) 2024 by lxulxu, All Rights Reserved. 
@@ -22,11 +22,11 @@ import requests
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
 
-logging.basicConfig(filename='movie_scraper.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+logging.basicConfig(filename='media_scraper.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"})
 
-def fetch_movie_details(url): 
+def fetch_media_details(url): 
     try: 
         time.sleep(random.uniform(1, 3))
 
@@ -34,11 +34,23 @@ def fetch_movie_details(url):
         response.raise_for_status()
         
         soup = BeautifulSoup(response.text, 'html.parser')
+   
+        date_element = None
+        if "subject" in url:
+            name_element = soup.find('span', {'property': 'v:itemreviewed'})
+            media_name = name_element.get_text().strip() if name_element else "Unknown Media"
 
-        name_element = soup.find('span', {'property': 'v:itemreviewed'})
-        movie_name   = name_element.get_text() if name_element else "Unknown Movie"
+            date_element = soup.find('span', {'property': 'v:initialReleaseDate'})
+        elif "game" in url:
+            print(url)
+            name_element = soup.find('title')
+            media_name = name_element.get_text().strip() if name_element else "Unknown Media"
+     
+            date_dt_element = soup.find('dt', string=lambda x: x and '预计上市时间:' in x)
+            if date_dt_element:
+                date_element = date_dt_element.find_next_sibling('dd')
 
-        date_element = soup.find('span', {'property': 'v:initialReleaseDate'})
+                    
         release_date = None
         if date_element: 
             date_string = date_element.get_text()
@@ -46,7 +58,8 @@ def fetch_movie_details(url):
             if match: 
                 release_date = match.group(0)
 
-        return movie_name, release_date
+        return media_name, release_date
+
     except Exception as e:
         logging.error(f"Unexpected error for URL {url}: {str(e)}")
         return "Unknown Movie", None
@@ -58,24 +71,24 @@ def fetch_rss_feed(rss_url):
         logging.error(f"Error fetching RSS feed: {e}")
         return None     
 
-def update_movie_data(entry, data):
-    movie_link = entry['link']
-    if movie_link not in data:
-        movie_name, release_date = fetch_movie_details(movie_link)
-        data[movie_link] = {'name': movie_name, 'release_date': release_date}
+def update_media_data(entry, data):
+    media_link = entry['link']
+    if media_link not in data:
+        media_name, release_date = fetch_media_details(media_link)
+        data[media_link] = {'name': media_name, 'release_date': release_date}
 
-def fetch_and_update_movies(rss_url, cache_file='movies_data.json', max_attempts=10): 
+def fetch_and_update_media(rss_url, cache_file='media_data.json', max_attempts=10): 
     data = load_data(cache_file)
     
     feed = fetch_rss_feed(rss_url)
     if feed and feed.entries:
         for entry in feed.entries:
-            if "想看" in entry.title:
-                update_movie_data(entry, data)
+            if "想看" in entry.title or "想玩" in entry.title:
+                update_media_data(entry, data)
 
-        no_date_movies = [link for link, info in data.items() if not info.get('release_date')]
-        for movie_link in random.sample(no_date_movies, min(len(no_date_movies), max_attempts)):
-            update_movie_data({'link': movie_link}, data)
+        no_date_media = [link for link, info in data.items() if not info.get('release_date')]
+        for media_link in random.sample(no_date_media, min(len(no_date_media), max_attempts)):
+            update_media_data({'link': media_link}, data)
 
     save_data(data, cache_file)
     return data
@@ -90,7 +103,7 @@ def save_data(data, cache_file):
     with open(cache_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
-def generate_ics_file(data, ics_path="movies.ics"): 
+def generate_ics_file(data, ics_path="media.ics"): 
     calendar = Calendar()
     tz = pytz.timezone('Asia/Shanghai')
 
@@ -118,5 +131,5 @@ if __name__ == "__main__":
     if rss_url is None:
         logging.error("RSS_URL environment variable is not set.")
 
-    data = fetch_and_update_movies(rss_url=rss_url)
+    data = fetch_and_update_media(rss_url=rss_url)
     generate_ics_file(data)
